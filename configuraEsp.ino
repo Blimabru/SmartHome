4a1fcc082e21e29ead3c133d7660b3dc03eb94c5
Blimabru <bru.no@outlook.com.br>
2024-05-01 18:40:44 -0300

    Refatoração na configuração inicial do ESP8266

--- configuraEsp.ino
+++ configuraEsp.ino
@@ -6,10 +6,20 @@ void configuraEsp() {
   lcd.setCursor(0, 1);
   lcd.println("     Wi-Fi...   ");
 
+  Serial.println("-------------------------\n");
   Serial.println("Configurando ESP8266...\n");
 
-  // Definindo ESP8266 como modo Estação
-  alternarModoEsp();
+  limparBufferSerial(); // Limpa o buffer serial antes de enviar um novo comando AT
+  ESP8266_SERIAL.println("AT+RESTORE");
+  Serial.println("- Restaurando ESP:\n");
+  Serial.println(aguardarResposta(1));
+  Serial.println("\n");
+
+  limparBufferSerial(); // Limpa o buffer serial antes de enviar um novo comando AT
+  ESP8266_SERIAL.println("AT+CWMODE=1");
+  Serial.println("- Alterando para modo Estação:\n");
+  Serial.println(aguardarResposta(1));
+  Serial.println("-------------------------\n");
 
   // Conectando Wi-Fi
   conectaWifi();
