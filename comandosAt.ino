1b55e7b64899e027264134f6906bb1c30cd0d788
Blimabru <bru.no@outlook.com.br>
2024-05-01 18:39:30 -0300

    Refatoração da função para utilizar comandos AT através do Arduíno

--- comandosAt.ino
+++ comandosAt.ino
@@ -1,26 +1,20 @@
-void comandosAt()
-{
+void comandosAt() {
 
-    // Verificar se há comandos disponíveis no monitor serial
-    if (Serial.available() > 0)
-    {
-        // Ler o comando da porta serial
-        String command = Serial.readStringUntil('\n');
+  // Verificar se há comandos disponíveis no monitor serial
+  if (Serial.available() > 0) {
+    // Ler o comando da porta serial
+    String command = Serial.readStringUntil('\n');
 
-        // Enviar o comando para o ESP8266
-        ESP8266_SERIAL.println(command);
+    limparBufferSerial(); // Limpa o buffer serial antes de enviar um novo comando AT
+    ESP8266_SERIAL.println(command); // Enviar o comando para o ESP8266
+  }
 
-        // Aguardar um pouco para o ESP8266 responder
-        delay(1000);
+  // Verificar se há resposta do ESP8266
+  while (ESP8266_SERIAL.available()) {
+    char c = ESP8266_SERIAL.read();
+    Serial.write(c);  // Imprimir resposta do ESP8266 no monitor serial
+  }
 
-        // Verificar se há resposta do ESP8266
-        while (ESP8266_SERIAL.available())
-        {
-            char c = ESP8266_SERIAL.read();
-            Serial.write(c); // Imprimir resposta do ESP8266 no monitor serial
-        }
-    }
-
-    // Aguardar um pouco antes de enviar outro comando
-    delay(1000);
+  // Aguardar um pouco antes de enviar outro comando
+  delay(1000);
 }
